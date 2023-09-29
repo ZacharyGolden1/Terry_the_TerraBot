2b37c24d694ad40085f4ce6c247b139deaf21356
ZacharyGolden1 <golden13911@gmail.com>
2023-09-29 14:30:40 -0400

    fully working diagnosis

---
+++ Refresh_Token.py
@@ -0,0 +1,28 @@
+# https://accounts.google.com/o/oauth2/auth?client_id=623191355592-m31r1l98oi7r60ur695vjn2crrbl9do9.apps.googleusercontent.com&redirect_uri=https://accounts.google.com&scope=https://www.googleapis.com/auth/userinfo.email&response_type=code
+# https://myaccount.google.com/u/1/?pli=1
+
+import requests
+
+SCOPE = 'https://www.googleapis.com/auth/userinfo.email'
+CLIENT_ID = '623191355592-m31r1l98oi7r60ur695vjn2crrbl9do9.apps.googleusercontent.com'
+CLIENT_SECRET = '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'
+REFRESH_TOKEN = "1//0d12S8CyPZKbRCgYIARAAGA0SNwF-L9IrsBVuxrC_AHO8Mr0i6U4aiezzOIT87QkauyyUuVUbQ3ZxwZ3Ji4Uk2ZtxEWXQvsoUAI4"
+REDIRECT_URI = 'https://accounts.google.com'
+AUTHORIZATION_CODE = "4%2F0AfJohXn6kgQhXeAspHZx_FuFR_u0Pb1gNbsOZJsnx9oT6CsFknnco7qx7dgN3GiAzFWuqw"
+
+
+token_url = 'https://accounts.google.com/o/oauth2/token'
+data = {
+    'code': AUTHORIZATION_CODE,
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+    'redirect_uri': REDIRECT_URI,
+    'grant_type': 'authorization_code'
+}
+
+response = requests.post(token_url, data=data)
+response_data = response.json()
+
+print(response_data)
+
+
--- agents/Mon_HW/diagnosis.py
+++ agents/Mon_HW/diagnosis.py
@@ -28,21 +28,95 @@ def create_working_relations(model, variables):
 
 def create_connected_relations(model, variables):
     # BEGIN STUDENT CODE
+    connected_relations = [
+    connected("H-T0", "Sensor-Board0"),
+    connected("Light0", "Sensor-Board0"),
+    connected("Moisture0", "Sensor-Board0"),
+    connected("H-T1", "Sensor-Board1"),
+    connected("Light1", "Sensor-Board1"),
+    connected("Moisture1", "Sensor-Board1"),
+    connected("Sensor-Board0", "Arduino"),
+    connected("Sensor-Board1", "Arduino"),
+    connected("Wlevel", "Arduino"),
+    connected("Arduino", "Power-Board"),
+    connected("Arduino", "Rasp-Pi"),
+    connected("Rasp-Pi", "Arduino"),
+    connected("Power-Board", "Fans"),
+    connected("Power-Board", "Pump"),
+    connected("Outlet", "Rasp-Pi"),
+    connected("Outlet", "Power-Board"),
+    connected("Power-Board", "LEDs")
+]
+    create_relations(connected_relations, model, variables)
+    
+    
+    
+    
     # END STUDENT CODE
     pass
 
 def create_powered_relations(model, variables):
     # BEGIN STUDENT CODE
+    powerRelations = [powered('Outlet'), powered('Rasp-Pi'),powered('Rasp-Pi'),powered('Power-Board'),powered('Fans'),powered('LEDs'),powered('Pump')]
+    
+    
+    create_relations(powerRelations, model, variables)
+
     # END STUDENT CODE
-    pass
+    
 
 def create_signal_relations(model, variables):
     # BEGIN STUDENT CODE
+    signal_relations = [
+    signal("H-T0", "H-T0"),
+    signal("H-T0", "Sensor-Board0"),
+    signal("H-T0", "Arduino"),
+    signal("H-T0", "Rasp-Pi"),
+    signal("Light0", "Light0"),
+    signal("Light0", "Sensor-Board0"),
+    signal("Light0", "Arduino"),
+    signal("Light0", "Rasp-Pi"),
+    signal("Moisture0", "Moisture0"),
+    signal("Moisture0", "Sensor-Board0"),
+    signal("Moisture0", "Arduino"),
+    signal("Moisture0", "Rasp-Pi"),
+    signal("H-T1", "H-T1"),
+    signal("H-T1", "Sensor-Board1"),
+    signal("H-T1", "Arduino"),
+    signal("H-T1", "Rasp-Pi"),
+    signal("Light1", "Light1"),
+    signal("Light1", "Sensor-Board1"),
+    signal("Light1", "Arduino"),
+    signal("Light1", "Rasp-Pi"),
+    signal("Moisture1", "Moisture1"),
+    signal("Moisture1", "Sensor-Board1"),
+    signal("Moisture1", "Arduino"),
+    signal("Moisture1", "Rasp-Pi"),
+    signal("Wlevel", "Wlevel"),
+    signal("Wlevel", "Arduino"),
+    signal("Wlevel", "Rasp-Pi"),
+    signal("LEDs", "Rasp-Pi"),
+    signal("LEDs", "Arduino"),
+    signal("LEDs", "Power-Board"),
+    signal("Fans", "Rasp-Pi"),
+    signal("Fans", "Arduino"),
+    signal("Fans", "Power-Board"),
+    signal("Pump", "Rasp-Pi"),
+    signal("Pump", "Arduino"),
+    signal("Pump", "Power-Board")
+]
+    create_relations(signal_relations, model, variables)
     # END STUDENT CODE
     pass
 
 def create_expected_result_relations(model, variables):
     # BEGIN STUDENT CODE
+    expected_result_relations = [
+    expected_result("Fans"),
+    expected_result("LEDs"),
+    expected_result("Pump")
+]
+    create_relations(expected_result_relations, model, variables)
     # END STUDENT CODE
     pass
 
@@ -83,16 +157,134 @@ def create_powered_constraints(model, variables):
 
 def create_signal_constraints(model, variables):
     # BEGIN STUDENT CODE
+    relations = [('HT-0', None)]
+    
+    for sen in ['H-T0', 'Light0', 'Moisture0']:
+        sensorBoard = 'Sensor-Board0'
+        constraint = ("IFF('%s', AND('%s', AND('%s',OR('%s','%s'))))" % 
+                        (signal(sen,sensorBoard), 	    
+                        connected(sen,sensorBoard),
+                        working(sen),
+                        signal(sen,sen),                    
+                        rasp_pi_signal(sen)))
+        add_constraint_to_model(constraint,model,variables)
+    
+    
+    for sen in ['H-T1', 'Light1', 'Moisture1']:
+        sensorBoard = 'Sensor-Board1'
+        constraint = ("IFF('%s', AND('%s', AND('%s',OR('%s','%s'))))" % 
+                        (signal(sen,sensorBoard), 	    
+                        connected(sen,sensorBoard),
+                        working(sen),
+                        signal(sen,sen),
+                        rasp_pi_signal(sen)))
+        add_constraint_to_model(constraint,model,variables)
+        
+    
+    sensor = 'Wlevel'
+    constraint = ("IFF('%s', AND('%s', OR('%s', '%s')))" %
+                  (signal(sensor, 'Arduino'),    
+                   connected(sensor, 'Arduino'),
+                   signal(sensor, sensor),
+                   rasp_pi_signal(sensor)))
+    add_constraint_to_model(constraint, model, variables) 
+    
+  
+    for actuator in actuators:
+        object1 = 'Arduino'
+        object2 = 'Rasp-Pi'
+        constraint = ("IFF('%s', AND('%s', AND('%s','%s')))" %
+                          (signal(actuator, object1),
+                           connected(object2, object1),
+                           working(object2),
+                           signal(actuator,object2))) 
+        
+        add_constraint_to_model(constraint, model, variables)
+    
+    for actuator in actuators:
+        object1 = 'Power-Board'
+        object2 = 'Arduino'
+        constraint = ("IFF('%s', AND('%s', AND('%s','%s')))" %
+                          (signal(actuator, object1),
+                           connected(object2, object1),
+                           working(object2),
+                           signal(actuator,object2))) 
+        add_constraint_to_model(constraint, model, variables)
+    
+    
+    
+    
+   
+    for sensor in sensors:
+        object1 = 'Arduino'
+        constraint = ("IFF('%s', AND('%s', AND('%s','%s')))" % 
+                        (signal(sensor,'Rasp-Pi'), 	    
+                        connected('Arduino','Rasp-Pi'),
+                        signal(sensor,'Arduino'),
+                        working('Arduino')))
+        
+        add_constraint_to_model(constraint, model, variables)
+    
+    #problems
+    for sensor in ['H-T1', 'Light1', 'Moisture1']:
+        constraint = ("IFF('%s', AND('%s', AND('%s','%s')))" % 
+                        (signal(sensor,'Arduino'), 	    
+                        connected('Sensor-Board1','Arduino'),
+                        signal(sensor,'Sensor-Board1'),
+                        working('Sensor-Board1')))
+        
+        add_constraint_to_model(constraint, model, variables)
+    
+    #problems
+    for sensor in ['H-T0', 'Light0', 'Moisture0']:
+        constraint = ("IFF('%s', AND('%s', AND('%s','%s')))" % 
+                        (signal(sensor,'Arduino'), 	    
+                        connected('Sensor-Board0','Arduino'),
+                        signal(sensor,'Sensor-Board0'),
+                        working('Sensor-Board0')))
+        
+        add_constraint_to_model(constraint, model, variables)
+        
+       
+    
     # END STUDENT CODE
-    pass
+    
 
 def create_sensor_generation_constraints(model, variables):
     # BEGIN STUDENT CODE
+    for sensor in sensors:
+    	constraint = "IFF('%s', '%s')" %(signal(sensor,sensor),working(sensor))
+    	add_constraint_to_model(constraint, model, variables)
+    
     # END STUDENT CODE
-    pass
+    
 
 def create_expected_result_constraints(model, variables):
     # BEGIN STUDENT CODE
+    leds_constraint = ("IFF('%s', AND('%s', AND('%s', OR('%s', '%s'))))"
+                       % (expected_result('LEDs'),
+                          powered('LEDs'),
+                          working('LEDs'),rasp_pi_signal('Light0'),rasp_pi_signal('Light1')))
+    add_constraint_to_model(leds_constraint, model, variables)
+    
+    fans_constraint = ("IFF('%s', AND('%s', AND('%s', OR('%s', '%s'))))"
+                       % (expected_result('Fans'),
+                          powered('Fans'),
+                          working('Fans'),rasp_pi_signal('H-T0'),rasp_pi_signal('H-T1')))
+    add_constraint_to_model(fans_constraint, model, variables)
+    
+    
+    pump_constraint = ("IFF('%s', AND('%s', AND('%s', OR('%s', OR('%s','%s')))))"
+                       % (expected_result('Pump'),
+                          powered('Pump'),
+                          working('Pump'),rasp_pi_signal('Moisture0'),rasp_pi_signal('Moisture1'),
+                          rasp_pi_signal('Wlevel')))
+    add_constraint_to_model(pump_constraint, model, variables)
+    
+    
+    
+    
+    
     # END STUDENT CODE
     pass
 
@@ -117,25 +309,36 @@ class DiagnosesCollector(cp_model.CpSolverSolutionCallback):
     def __init__(self, variables):
         cp_model.CpSolverSolutionCallback.__init__(self)
         # BEGIN STUDENT CODE
+        self.variables = variables
+        self.diagnoses = []
+        
         # END STUDENT CODE
 
     def OnSolutionCallback(self):
         # Extract the connected and working relations that are False
         # BEGIN STUDENT CODE
+        self.diagnoses.append(collect_diagnosis(self,self.variables))
         # END STUDENT CODE
-        pass
+        
 
 def diagnose(observations):
     model, variables = create_greenhouse_model()
     add_constraint_to_model(observations, model, variables)
 
     collector = DiagnosesCollector(variables)
-    diagnoses = []
     solver = cp_model.CpSolver()
     solver.SearchForAllSolutions(model, collector)
-    # Remove all redundant diagnoses (those that are supersets
-    #   of other diagnoses).
+    
+    # Remove all redundant diagnoses (those that are supersets of other diagnoses).
     # BEGIN STUDENT CODE
-    # END STUDENT CODE
+    diagnoses = sorted(collector.diagnoses,key=len)
+    res = []
+    for dia in diagnoses:
+        flag = True
+        for item in res:
+            if dia.issuperset(item):
+                flag = False
+        if flag: res+=[dia]
 
-    return diagnoses
+    
+    return res
